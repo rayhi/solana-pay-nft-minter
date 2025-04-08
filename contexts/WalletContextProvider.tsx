3da7a176beb8203288bcff26a674ffb877d97cb0
rayhi <rayhisg38@gmail.com>
2025-04-08 12:42:41 +0300

    Update WalletContextProvider.tsx

--- contexts/WalletContextProvider.tsx
+++ contexts/WalletContextProvider.tsx
@@ -10,7 +10,7 @@ import { clusterApiUrl } from "@solana/web3.js";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const network = WalletAdapterNetwork.Mainnet;
+  const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   const wallets = useMemo(() => [new SolflareWalletAdapter()], [network]);
